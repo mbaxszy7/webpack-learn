107b67ada873a22ffdbfe2f22fdb0231820a7db4
zheyu yan <390016746@qq.com>
2020-06-14 22:09:32 +0800

    webpack use

---
+++ src/hello.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Hello = () => <div>hello qzj</div>;
+
+export default Hello;
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+module.exports = {
+  entry: path.join(__dirname, "./src/index.js"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
